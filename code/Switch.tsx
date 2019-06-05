6b5541c7a2b6bcc41697a736f6ebb22fc43dba36
Artem <asci@yandex.ru>
2019-06-05 14:52:24 +0200

    Another prettier round

--- code/Switch.tsx
+++ code/Switch.tsx
@@ -1,83 +1,83 @@
-import * as React from "react"
-import { PropertyControls, ControlType } from "framer"
+import * as React from 'react';
+import { PropertyControls, ControlType } from 'framer';
 // @ts-ignore: import works well, but editor can't resolve it
-import { default as MuiSwitch } from "@material-ui/core/Switch"
+import { default as MuiSwitch } from '@material-ui/core/Switch';
 
 interface Props {
-    checked: boolean
-    color: "primary" | "secondary" | "default"
-    disabled: boolean
-    disableRipple: boolean
+  checked: boolean;
+  color: 'primary' | 'secondary' | 'default';
+  disabled: boolean;
+  disableRipple: boolean;
 
-    onChange: (checked: boolean) => void
-    width: number
-    height: number
+  onChange: (checked: boolean) => void;
+  width: number;
+  height: number;
 }
 interface State {
-    checked: boolean
+  checked: boolean;
 }
 
 export class Switch extends React.Component<Props, State> {
-    static defaultProps = {
-        width: 30,
-        height: 30,
-        disabled: false,
-        disableRipple: true,
-        checked: false,
-    }
+  static defaultProps = {
+    width: 30,
+    height: 30,
+    disabled: false,
+    disableRipple: true,
+    checked: false,
+  };
 
-    static propertyControls: PropertyControls<Props> = {
-        checked: { type: ControlType.Boolean, title: "Checked" },
-        disabled: { type: ControlType.Boolean, title: "Disabled" },
-        disableRipple: { type: ControlType.Boolean, title: "Ripple" },
-        color: {
-            type: ControlType.Enum,
-            title: "Color",
-            options: ["primary", "secondary"],
-        },
-    }
+  static propertyControls: PropertyControls<Props> = {
+    checked: { type: ControlType.Boolean, title: 'Checked' },
+    disabled: { type: ControlType.Boolean, title: 'Disabled' },
+    disableRipple: { type: ControlType.Boolean, title: 'Ripple' },
+    color: {
+      type: ControlType.Enum,
+      title: 'Color',
+      options: ['primary', 'secondary'],
+    },
+  };
 
-    static getDerivedStateFromProps(nextProps: Props): State {
-        return {
-            checked: nextProps.checked,
-        }
-    }
+  static getDerivedStateFromProps(nextProps: Props): State {
+    return {
+      checked: nextProps.checked,
+    };
+  }
 
-    state = {
-        checked: false,
-    }
+  state = {
+    checked: false,
+  };
 
-    onTap = () => {
-        if (this.props.onChange) {
-            this.props.onChange(!this.state.checked)
-        } else {
-            this.setState({
-                checked: !this.state.checked,
-            })
-        }
+  onTap = () => {
+    if (this.props.onChange) {
+      this.props.onChange(!this.state.checked);
+    } else {
+      this.setState({
+        checked: !this.state.checked,
+      });
     }
+  };
 
-    render() {
-        const { width, height } = this.props
-        const { color, disabled, disableRipple } = this.props
-        return (
-            <div
-                style={{
-                    width,
-                    height,
-                    display: "flex",
-                    alignItems: "center",
-                    justifyContent: "center",
-                }}
-            >
-                <MuiSwitch
-                    checked={this.state.checked}
-                    color={color}
-                    disabled={disabled}
-                    disableRipple={!disableRipple}
-                    onChange={this.onTap}
-                />
-            </div>
-        )
-    }
+  render() {
+    const { width, height } = this.props;
+    const { color, disabled, disableRipple } = this.props;
+    return (
+      <div
+        style={{
+          width,
+          height,
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <MuiSwitch
+          checked={this.state.checked}
+          color={color}
+          disabled={disabled}
+          disableRipple={!disableRipple}
+          onChange={this.onTap}
+        />
+      </div>
+    );
+  }
 }
