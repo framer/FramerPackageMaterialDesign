692f2b64bde06bb6b65f3d2a07d27c236cdc852a
Artem <asci@yandex.ru>
2019-06-05 12:13:23 +0200

    Another prettier run + ts ignore comments

--- code/Button.tsx
+++ code/Button.tsx
@@ -1,93 +1,94 @@
-import * as React from 'react';
-import { PropertyControls, ControlType } from 'framer';
-import { default as MuiButton } from '@material-ui/core/Button';
+import * as React from "react"
+import { PropertyControls, ControlType } from "framer"
+// @ts-ignore: import works well, but editor can't resolve it
+import { default as MuiButton } from "@material-ui/core/Button"
 
 interface Props {
-  color: 'primary' | 'secondary' | 'default';
-  size: 'small' | 'medium' | 'large';
-  variant: 'text' | 'flat' | 'outlined' | 'contained' | 'raised' | 'fab';
-  href: string;
-  text: string;
-  disabled: boolean;
-  disableRipple: boolean;
-  fullWidth: boolean;
-  width: number;
-  height: number;
-  onClick: () => void;
+    color: "primary" | "secondary" | "default"
+    size: "small" | "medium" | "large"
+    variant: "text" | "flat" | "outlined" | "contained" | "raised" | "fab"
+    href: string
+    text: string
+    disabled: boolean
+    disableRipple: boolean
+    fullWidth: boolean
+    width: number
+    height: number
+    onClick: () => void
 }
 
 export class Button extends React.Component<Props> {
-  static defaultProps = {
-    width: 101,
-    height: 40,
+    static defaultProps = {
+        width: 101,
+        height: 40,
 
-    color: 'primary',
-    size: 'medium',
-    variant: 'raised',
-    href: '',
-    text: 'Click',
-    disabled: false,
-    disableRipple: false,
-    fullWidth: false,
-  };
+        color: "primary",
+        size: "medium",
+        variant: "raised",
+        href: "",
+        text: "Click",
+        disabled: false,
+        disableRipple: false,
+        fullWidth: false,
+    }
 
-  static propertyControls: PropertyControls<Props> = {
-    text: { type: ControlType.String, title: 'Title' },
-    href: { type: ControlType.String, title: 'URL' },
-    disabled: { type: ControlType.Boolean, title: 'Disabled' },
-    disableRipple: { type: ControlType.Boolean, title: 'Ripple' },
-    fullWidth: { type: ControlType.Boolean, title: 'Full Width' },
-    color: {
-      type: ControlType.Enum,
-      title: 'Color',
-      options: ['primary', 'secondary', 'default'],
-    },
-    size: {
-      type: ControlType.Enum,
-      title: 'Size',
-      options: ['small', 'medium', 'large'],
-    },
-    variant: {
-      type: ControlType.Enum,
-      title: 'Variant',
-      options: ['text', 'flat', 'outlined', 'contained', 'raised', 'fab'],
-    },
-  };
+    static propertyControls: PropertyControls<Props> = {
+        text: { type: ControlType.String, title: "Title" },
+        href: { type: ControlType.String, title: "URL" },
+        disabled: { type: ControlType.Boolean, title: "Disabled" },
+        disableRipple: { type: ControlType.Boolean, title: "Ripple" },
+        fullWidth: { type: ControlType.Boolean, title: "Full Width" },
+        color: {
+            type: ControlType.Enum,
+            title: "Color",
+            options: ["primary", "secondary", "default"],
+        },
+        size: {
+            type: ControlType.Enum,
+            title: "Size",
+            options: ["small", "medium", "large"],
+        },
+        variant: {
+            type: ControlType.Enum,
+            title: "Variant",
+            options: ["text", "flat", "outlined", "contained", "raised", "fab"],
+        },
+    }
 
-  render() {
-    const {
-      text,
-      href,
-      disabled,
-      disableRipple,
-      fullWidth,
-      color,
-      size,
-      variant,
-    } = this.props;
-    const { width, height } = this.props;
-    return (
-      <div
-        style={{
-          width,
-          height,
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}
-      >
-        <MuiButton
-          href={href}
-          disabled={disabled}
-          disableRipple={disableRipple}
-          fullWidth={fullWidth}
-          color={color}
-          size={size}
-          variant={variant}
-        >
-          {text || Button.defaultProps.text}
-        </MuiButton>
-      </div>
-    );
-  }
+    render() {
+        const {
+            text,
+            href,
+            disabled,
+            disableRipple,
+            fullWidth,
+            color,
+            size,
+            variant,
+        } = this.props
+        const { width, height } = this.props
+        return (
+            <div
+                style={{
+                    width,
+                    height,
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                }}
+            >
+                <MuiButton
+                    href={href}
+                    disabled={disabled}
+                    disableRipple={disableRipple}
+                    fullWidth={fullWidth}
+                    color={color}
+                    size={size}
+                    variant={variant}
+                >
+                    {text || Button.defaultProps.text}
+                </MuiButton>
+            </div>
+        )
+    }
 }
